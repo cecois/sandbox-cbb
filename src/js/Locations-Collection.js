7f96b708a50d98e1fb7d900174e6219d0e154440
Cecois <cecmcgee@gmail.com>
2017-09-26 21:05:21 -0400

    syntax killer

--- src/js/Locations-Collection.js
+++ src/js/Locations-Collection.js
@@ -6,27 +6,30 @@ var Locations = Backbone.Collection.extend({
 	}
 	,url: function(){
 
-		var u =null;
+		// var u =null;
 
-		switch(CONFIG.mode) {
-			case 'T':
-		u = 'http://localhost:8983/solr/cbb_carto/select?wt=json&q='+encodeURI(this.map(function(m){return "cartodb_id:"+UTIL.geom_id_nudge(m.get("location_type"),m.get("location_id"),"up")}).join(" OR "));
-					break;
-					default:
-					u = CONFIG.index_root+'indent=off&rows='+appQuery.get("numRows")+'&json.wrf=cwmccallback&wt=json&fl=_id,episode,slug_earwolf,bit,instance,location_type,location_id,created_at,updated_at,elucidation,tags&q=(holding:false) AND '+encodeURI(appQuery.get("querystring"))
-					+'&'+appQuery.get("facetstring")+'&facet.query='+encodeURI(appQuery.get("querystring"))
-				}
+		var u= "http://localhost:8983/solr/cbb_carto/select?wt=json&q=cartodb_id:829917"
+
+		// switch(CONFIG.mode) {
+		// 	case 'T':
+		// u = 'http://localhost:8983/solr/cbb_carto/select?wt=json&q='+encodeURI(this.map(function(m){return "cartodb_id:"+UTIL.geom_id_nudge(m.get("location_type"),m.get("location_id"),"up")}).join(" OR "));
+		// 			break;
+		// 			default:
+		// 			u = CONFIG.index_root+'indent=off&rows='+appQuery.get("numRows")+'&json.wrf=cwmccallback&wt=json&fl=_id,episode,slug_earwolf,bit,instance,location_type,location_id,created_at,updated_at,elucidation,tags&q=(holding:false) AND '+encodeURI(appQuery.get("querystring"))
+		// 			+'&'+appQuery.get("facetstring")+'&facet.query='+encodeURI(appQuery.get("querystring"))
+		// 		}
 
 				return u
 
 	}
 	,sync: function(method, collection, options) {
-
+console.log("26, sync");
 		options.dataType = "jsonp";
 		options.jsonpCallback = 'cwmccallback';
 		return Backbone.sync(method, collection, options)
 	}
 	,parse: function(data) {
+console.log("32, parse");
 
 		// heres wehre where z ip back with bits?
 
